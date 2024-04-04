0bf64812eedf9ea6e4173b416e87590d6dccfb70
Julie Aymar <62514932+Keir-oriyerdan@users.noreply.github.com>
2024-04-04 15:06:51 +0200

    first commit

---
+++ conception/README.md
@@ -0,0 +1,9 @@
+# Projet1-Groupe3
+```shell
+Lilya Maylis Badr Julie
+```
+
+# Test Julie
+```shell
+git stash
+```
---
+++ script/Contact.js
@@ -0,0 +1,3 @@
+import Contact from "./components/Contact.js";
+// new Contact("John", "Doe", "Influenceur", "johndoe@email.fr", "0612345678", "bonjour")
+new Contact();
